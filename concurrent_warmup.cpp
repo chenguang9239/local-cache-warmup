52ad870929c09a1bc01720e24cffcb72cd2e0839
wangzhiyi <wangzhiyi@yidian-inc.com>
2020-09-04 01:38:36 +0800

    add concurrent dump/warmup util

---
+++ concurrent_warmup.cpp
@@ -0,0 +1,288 @@
+#include "concurrent_warmup.h"
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <thread>
+
+namespace ww {
+
+static size_t max_size = 1024 * 1024;
+static size_t total_size = max_size * 2;
+
+std::string ErrStr1(int err_no) {
+  char *e = strerror(errno);
+  return e ? e : "no error message";
+}
+
+size_t Size(const std::string &file_name) {
+  FILE *fp = fopen(file_name.c_str(), "r");
+  if (fp == NULL) {
+    LOG_ERROR << "open file error";
+    return -1;
+  }
+
+  fseek(fp, 0L, SEEK_END);
+  size_t res = ftell(fp);
+
+  fclose(fp);
+  return res;
+}
+
+struct OutputBuffer {
+  OutputBuffer() {
+    ptr = nullptr;
+    fd = -1;
+    cur_pos = 0;
+  };
+
+  OutputBuffer(const std::string &file_name, size_t n) {
+    cur_pos = 0;
+    ptr = (char *)malloc(n);
+    if (!ptr) {
+      LOG_ERROR << "malloc " << n << ", error: " << ErrStr1(errno);
+    }
+
+    fd = open(file_name.c_str(), O_CREAT | O_WRONLY);
+    if (fd < 0) {
+      LOG_ERROR << "open file error: " << file_name;
+    }
+  }
+
+  ~OutputBuffer() {
+    if (ptr)
+      free(ptr);
+    if (fd > 0)
+      close(fd);
+  }
+
+  OutputBuffer(OutputBuffer &&other) {
+    ptr = other.ptr;
+    fd = other.fd;
+    cur_pos = other.cur_pos;
+
+    other.ptr = nullptr;
+    other.fd = -1;
+    other.cur_pos = 0;
+  }
+
+  OutputBuffer &operator=(OutputBuffer &&other) {
+    ptr = other.ptr;
+    fd = other.fd;
+    cur_pos = other.cur_pos;
+
+    other.ptr = nullptr;
+    other.fd = -1;
+    other.cur_pos = 0;
+    return *this;
+  }
+
+  OutputBuffer(const OutputBuffer &) = delete;
+  OutputBuffer &operator=(const OutputBuffer &) = delete;
+
+  char *ptr;
+  int fd;
+  size_t cur_pos;
+};
+
+struct InputBuffer {
+  InputBuffer() {
+    ptr = nullptr;
+    size = 0;
+    cur_pos = 0;
+  };
+
+  InputBuffer(const std::string file_name) {
+    cur_pos = 0;
+    if (0 == access(file_name.c_str(), R_OK)) {
+      int fd = open(file_name.c_str(), O_RDONLY);
+      if (fd < 0) {
+        LOG_ERROR << "open file failed: " << file_name;
+        return;
+      }
+      size = Size(file_name.c_str());
+      ptr = (char *)mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0);
+      if (ptr) {
+        LOG_INFO << "mmap ok, ptr: " << (void *)ptr << ", size: " << size;
+      } else {
+        LOG_ERROR << "mmap error: " << ErrStr1(errno);
+      }
+      close(fd);
+    }
+  }
+
+  ~InputBuffer() {
+    if (ptr) {
+      if (0 == munmap(ptr, size)) {
+        LOG_INFO << "munmap ok, ptr: " << (void *)ptr << ", size: " << size;
+      } else {
+        LOG_ERROR << "munmap error: " << ErrStr1(errno)
+                  << ", ptr: " << (void *)ptr << ", size: " << size;
+        ;
+      }
+    }
+  }
+
+  InputBuffer(InputBuffer &&other) {
+    ptr = other.ptr;
+    cur_pos = other.cur_pos;
+    size = other.size;
+
+    other.ptr = nullptr;
+    other.cur_pos = 0;
+    other.size = 0;
+  }
+
+  InputBuffer &operator=(InputBuffer &&other) {
+    ptr = other.ptr;
+    cur_pos = other.cur_pos;
+    size = other.size;
+
+    other.ptr = nullptr;
+    other.cur_pos = 0;
+    other.size = 0;
+    return *this;
+  }
+
+  InputBuffer(const OutputBuffer &) = delete;
+  InputBuffer &operator=(const OutputBuffer &) = delete;
+
+  char *ptr;
+  size_t size;
+  size_t cur_pos;
+};
+
+thread_local OutputBuffer wt_buf;
+thread_local InputBuffer rd_buf;
+
+int Write(const void *buf, size_t bytes, bool immediate) {
+  if (!wt_buf.ptr) {
+    if (write(wt_buf.fd, buf, bytes) < 0) {
+      LOG_ERROR << "write error: " << ErrStr1(errno);
+      return -1;
+    }
+    return 0;
+  }
+
+  if (wt_buf.cur_pos >= max_size || (immediate && wt_buf.cur_pos > 0)) {
+    if (write(wt_buf.fd, wt_buf.ptr, wt_buf.cur_pos) < 0) {
+      LOG_ERROR << "write error: " << ErrStr1(errno);
+      return -1;
+    }
+    wt_buf.cur_pos = 0;
+  }
+
+  if (buf) {
+    if (bytes <= max_size) {
+      memcpy(wt_buf.ptr + wt_buf.cur_pos, buf, bytes);
+      wt_buf.cur_pos += bytes;
+    } else {
+      if (write(wt_buf.fd, buf, bytes) < 0) {
+        LOG_ERROR << "write error: " << ErrStr1(errno);
+        return -1;
+      }
+    }
+  }
+
+  return 0;
+}
+
+int Read(void *target, size_t bytes) {
+  if (!rd_buf.ptr) {
+    LOG_ERROR << "null read buffer ptr";
+    return -2;
+  }
+
+  size_t new_pos = rd_buf.cur_pos + bytes;
+  if (new_pos > rd_buf.size) {
+    return -1; // end
+  }
+
+  memcpy(target, rd_buf.ptr + rd_buf.cur_pos, bytes);
+  rd_buf.cur_pos = new_pos;
+
+  return 0;
+}
+
+int DoWriteTask(WriteTaskFunc write_task_func, const std::string &file_name) {
+  wt_buf = OutputBuffer(file_name, total_size);
+
+  LOG_DEBUG << "buffer addr: " << (void *)wt_buf.ptr
+            << ", write cur pos addr: " << &wt_buf.cur_pos;
+  return write_task_func();
+}
+
+int ReadTask(QuickDeserializeFunc deserialize_func,
+             const std::string &file_name) {
+  rd_buf = InputBuffer(file_name);
+
+  LOG_DEBUG << "rd buffer addr: " << (void *)rd_buf.ptr
+            << ", read cur pos addr: " << &rd_buf.cur_pos
+            << ", file name: " << file_name;
+
+  return deserialize_func();
+}
+
+int ConcurrentWarmup(DeserializeFunc deserialize_func,
+                     const std::string &file_name, int thread_cnt) {
+  if (thread_cnt <= 0) {
+    thread_cnt = std::thread::hardware_concurrency();
+  }
+  if (thread_cnt > 100) {
+    thread_cnt = 100;
+  }
+
+  ThreadPool thread_pool(thread_cnt);
+  std::vector<std::future<int>> futures;
+
+  for (int i = 0; i < 1000; ++i) {
+    auto split_name = file_name + std::to_string(i);
+    if (0 == access(split_name.c_str(), R_OK)) {
+      futures.emplace_back(thread_pool.enqueue(deserialize_func, split_name));
+    } else {
+      break;
+    }
+  }
+
+  for (auto &future : futures) {
+    future.wait();
+  }
+
+  return 0;
+}
+
+int ConcurrentWarmup(QuickDeserializeFunc deserialize_func,
+                     const std::string &file_name, int thread_cnt) {
+  if (thread_cnt <= 0) {
+    thread_cnt = std::thread::hardware_concurrency();
+  }
+  if (thread_cnt > 100) {
+    thread_cnt = 100;
+  }
+
+  ThreadPool thread_pool(thread_cnt);
+  std::vector<std::future<int>> futures;
+
+  for (int i = 0; i < 1000; ++i) {
+    auto split_name = file_name + std::to_string(i);
+    if (0 == access(split_name.c_str(), R_OK)) {
+      futures.emplace_back(
+          thread_pool.enqueue(&ReadTask, deserialize_func, split_name));
+    } else {
+      break;
+    }
+  }
+
+  for (auto &future : futures) {
+    future.wait();
+  }
+
+  return 0;
+}
+
+} // namespace ww
---
+++ concurrent_warmup.h
@@ -0,0 +1,124 @@
+#ifndef CONCURRENT_WARMUP_H
+#define CONCURRENT_WARMUP_H
+
+#include <unistd.h>
+
+#include <functional>
+#include <future>
+#include <memory>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+
+#include "cptl.h"
+
+#ifndef LOG_DEBUG
+#include <iostream>
+#define LOG_DEBUG std::cout << std::endl
+#endif
+
+#ifndef LOG_INFO
+#include <iostream>
+#define LOG_INFO std::cout << std::endl
+#endif
+
+#ifndef LOG_ERROR
+#include <iostream>
+#define LOG_ERROR std::cout << std::endl
+#endif
+
+namespace ww {
+
+template <typename K, typename V> using CacheType = std::unordered_map<K, V>;
+
+template <typename K, typename V>
+using SerializeFunc = std::function<int(const std::unordered_map<K, V> &)>;
+
+template <typename K, typename V>
+std::vector<CacheType<K, V>> SplitUnMap(const CacheType<K, V> &un_map, int n);
+
+using DeserializeFunc = std::function<int(const std::string &)>;
+using QuickDeserializeFunc = std::function<int()>;
+using WriteTaskFunc = std::function<int()>;
+
+using ThreadPool = cptl::thread_pool;
+
+int DoWriteTask(WriteTaskFunc write_task_func, const std::string &file_name);
+
+template <typename K, typename V>
+int ConcurrentDump(const CacheType<K, V> &dest_map,
+                   SerializeFunc<K, V> serialize_func,
+                   const std::string &file_name, int thread_cnt = 0);
+
+int ConcurrentWarmup(DeserializeFunc deserialize_func,
+                     const std::string &file_name, int thread_cnt = 0);
+int ConcurrentWarmup(QuickDeserializeFunc deserialize_func,
+                     const std::string &file_name, int thread_cnt = 0);
+
+int Write(const void *buf, size_t bytes, bool immediate = false);
+int Read(void *target, size_t bytes);
+
+template <typename K, typename V>
+std::vector<CacheType<K, V>> SplitUnMap(const CacheType<K, V> &un_map, int n) {
+  std::vector<std::unordered_map<K, V>> res;
+
+  if (un_map.size() <= n || n <= 0) {
+    res.emplace_back(un_map);
+    return res;
+  }
+
+  int i = 1;
+  int single_cnt = 1 + un_map.size() / n;
+  if (un_map.size() == n) {
+    --single_cnt;
+  }
+
+  std::unordered_map<K, V> tmp;
+  for (auto &entry : un_map) {
+    tmp[entry.first] = entry.second;
+    if (i++ % single_cnt == 0) {
+      res.emplace_back(std::move(tmp));
+    }
+  }
+
+  if (i % n != 0) {
+    res.emplace_back(std::move(tmp));
+  }
+
+  return res;
+}
+
+template <typename K, typename V>
+int ConcurrentDump(const CacheType<K, V> &dest_map,
+                   SerializeFunc<K, V> serialize_func,
+                   const std::string &file_name, int thread_cnt) {
+  if (thread_cnt <= 0) {
+    thread_cnt = std::thread::hardware_concurrency();
+  }
+  if (thread_cnt > 100) {
+    thread_cnt = 100;
+  }
+
+  auto caches = SplitUnMap(dest_map, thread_cnt);
+  ThreadPool thread_pool(thread_cnt);
+  std::vector<std::future<int>> futures;
+
+  int i = 0;
+  for (auto &cache : caches) {
+    auto write_task_func = [&, serialize_func, cache]() {
+      return serialize_func(cache);
+    };
+    futures.emplace_back(thread_pool.enqueue(&DoWriteTask, write_task_func,
+                                             file_name + std::to_string(i++)));
+  }
+
+  for (auto &future : futures) {
+    future.wait();
+  }
+
+  return 0;
+}
+
+} // namespace ww
+
+#endif // CONCURRENT_WARMUP_H
